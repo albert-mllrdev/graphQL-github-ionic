2d64bf930a68e107343b2f0d8568f1b1f180f3c5
User <albert.cruz@mllrdev.com>
2020-07-30 13:04:03 +0800

    dotenv for codegen token

--- setKeys.ts
+++ setKeys.ts
@@ -1,14 +1,15 @@
 const fs = require('fs');
-
+const { argv } = require('yargs');
 require('dotenv').config();
-const targetPath = `./src/environments/environment.ts`;
+
+const isCodeGen = argv.isCodeGen;
+const targetPath = (isCodeGen) ? `./codegen.yml` : `./src/environments/environment.ts`;
 let content = fs.readFileSync(targetPath, 'utf8');
 
-if (content.indexOf('<GITHUB_AUTH_TOKEN>') < 0){
-    process.exit(-1);
+if (content.indexOf('<GITHUB_AUTH_TOKEN>') >= 0){
+    content = content.replace('<GITHUB_AUTH_TOKEN>', process.env.GITHUB_AUTH_TOKEN)
+    .replace('<GITHUB_CLIENT_ID>', process.env.GITHUB_CLIENT_ID)
+    .replace('<GITHUB_SECRET>', process.env.GITHUB_SECRET);
+    fs.writeFileSync(targetPath, content);
 }
 
-content = content.replace('<GITHUB_AUTH_TOKEN>', process.env.GITHUB_AUTH_TOKEN)
-            .replace('<GITHUB_CLIENT_ID>', process.env.GITHUB_CLIENT_ID)
-            .replace('<GITHUB_SECRET>', process.env.GITHUB_SECRET);
-fs.writeFileSync(targetPath, content);
