95abe409121bcc35b9a12f41dc6f25e0c6faf78f
User <albert.cruz@mllrdev.com>
2020-07-30 13:15:53 +0800

    added message if missing on .env

--- setKeys.ts
+++ setKeys.ts
@@ -6,6 +6,16 @@ const isCodeGen = argv.isCodeGen;
 const targetPath = (isCodeGen) ? `./codegen.yml` : `./src/environments/environment.ts`;
 let content = fs.readFileSync(targetPath, 'utf8');
 
+if (!process.env.GITHUB_AUTH_TOKEN){
+    console.log(`
+    create .env file on root with the following entries:
+
+    GITHUB_AUTH_TOKEN=
+    GITHUB_CLIENT_ID=
+    GITHUB_SECRET=    
+    `);
+}
+
 if (content.indexOf('<GITHUB_AUTH_TOKEN>') >= 0){
     content = content.replace('<GITHUB_AUTH_TOKEN>', process.env.GITHUB_AUTH_TOKEN)
     .replace('<GITHUB_CLIENT_ID>', process.env.GITHUB_CLIENT_ID)
